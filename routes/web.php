d9fd2ccc49bd7390fd99bea43786422fc5ae409e
azka savir <rizqiirkhammaulana@gmail.com>
2024-08-25 13:38:53 +0700

    Update Profile

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SummaryController;
+
 
 Route::get('/', function () {
     $user = Auth::user();
@@ -16,11 +18,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+Route::get('/summary', [SummaryController::class, 'show'])->name('summary.show');
+Route::get('/summary/edit/{id}', [SummaryController::class, 'edit'])->name('summary.edit');
+Route::put('/summary/update/{id}', [SummaryController::class, 'update'])->name('summary.update');
+Route::post('/summary/store', [SummaryController::class, 'store'])->name('summary.store');
 
 // Email Verifications
 Route::get('/email/verify', function () {
