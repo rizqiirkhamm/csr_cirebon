2af3203b3729140c117008c24191ed8eebe33bec
RespawnHood <ammarzann@gmail.com>
2024-08-25 14:10:39 +0700

    update migrations

--- database/migrations/2024_08_24_065818_create_proyeks_table.php
+++ database/migrations/2024_08_24_065818_create_proyeks_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->date('tgl_awal');
             $table->date('tgl_akhir');
             $table->text('deskripsi');
-            $table->string('foto_proyek')->default('thumbnail.png');
+            $table->string('thumbnail')->default('thumbnail.png');
             $table->foreign('id_sektor')->references('id')->on('sektors')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('id_program')->references('id')->on('programs')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
